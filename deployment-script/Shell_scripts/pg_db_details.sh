98b77aff8f2f6cd1730bd839eb42314201bcc3f9
Sajed <sajid.jkd@gmail.com>
2018-05-17 16:38:10 +0530

    Update pg_db_details.sh
    updated

--- deployment-script/Shell_scripts/pg_db_details.sh
+++ deployment-script/Shell_scripts/pg_db_details.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PGHOST="localhost"
+PGHOST="testdb1.c2tskthlfglu.us-east-1.rds.amazonaws.com"
 PGPORT="5432"
 PGDATABASE="testdb"
-PGUSER="testdb"
-PGPASSWORD="testdb"
+PGUSER="root"
+PGPASSWORD="root1234"
 NO_OF_CORES=2
 PSQL="/usr/local/bin/psql"
 
-export PGPASSWORD=testdb
+export PGPASSWORD=root1234
 
 BASE_FOLDER='/Users/durgach/github/DP/test-db/testschema/Constraints/'
 echo 'Postgres code basefolder=' $BASE_FOLDER
