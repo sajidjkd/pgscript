d29eccf8acaa71153c4a50d946c7ab9170fdbf1b
Sajed <sajid.jkd@gmail.com>
2018-05-21 15:10:28 +0530

    Update test.sql

--- test-db/testschema/Tables/test.sql
+++ test-db/testschema/Tables/test.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS testschema.test(
     test_id                  INTEGER NOT NULL,
-    test_cd                  CHARACTER VARYING(200) NOT NULL,
+    test_cd                  CHARACTER VARYING(200) NOT NULL
     test_cd1                 CHARACTER VARYING(300) NOT NULL
 )
 WITH (
