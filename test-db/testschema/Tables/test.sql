c00e0a70b8b03e20e6abd7d8d0333c5b7bd02421
Sajed <sajid.jkd@gmail.com>
2018-05-21 12:26:15 +0530

    Update test.sql

--- test-db/testschema/Tables/test.sql
+++ test-db/testschema/Tables/test.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS testschema.test(
     test_id                  INTEGER NOT NULL,
-    test_cd                  CHARACTER VARYING(200) NOT NULL
+    test_cd                  CHARACTER VARYING(200) NOT NULL,
+    test_cd1                 CHARACTER VARYING(200) NOT NULL
 )
 WITH (
 OIDS=FALSE
